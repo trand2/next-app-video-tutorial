788110eddd6cfb288aea5822aae4b0d57984b6b3
Tyler Randolph <tylerrandolph123@gmail.com>
2022-11-14 12:31:59 -0700

    feat: implement home and results pages

--- components/layouts/primary/PrimaryLayout.tsx
+++ components/layouts/primary/PrimaryLayout.tsx
@@ -1,15 +1,26 @@
 import Head from 'next/head';
-import styles from './PrimaryLayout.module.css';
+import Footer from '../../navigation/footer/Footer';
+import Header from '../../navigation/header/Header';
 
-export interface IPrimaryLayout {}
+export interface IPrimaryLayout {
+  justify?: 'items-center' | 'items-start';
+}
 
-const PrimaryLayout: React.FC<IPrimaryLayout> = ({ children }) => {
+const PrimaryLayout: React.FC<IPrimaryLayout> = ({
+  children,
+  justify = 'items-center',
+}) => {
   return (
     <>
       <Head>
-        <title>Primary Layout Example</title>
+        <title>NextJs Fullstack App Template</title>
       </Head>
-      <main className={styles.main}>{children}</main>
+      <div className={`min-h-screen flex flex-col ${justify}`}>
+        <Header />
+        <main className="px-5">{children}</main>
+        <div className="m-auto" />
+        <Footer />
+      </div>
     </>
   );
 };
